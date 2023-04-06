8dced1b949881f2758306c2076421441d30acdda
Thomas AWOUNFOUET <thomas.awounfouet@gmail.com>
2023-04-06 22:09:59 +0200

    Optimisation de la structure du back-end - MVC

---
+++ backend/routes/stuff.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+
+
+const stuffCtrl = require('../controllers/stuff');
+
+
+router.post('/', stuffCtrl.createThing);
+router.put('/:id', stuffCtrl.modifyThing);
+router.delete('/:id', stuffCtrl.deleteThing);
+router.get('/:id', stuffCtrl.getOneThing);
+router.get('/', stuffCtrl.getAllStuff);
+
+
+module.exports = router;
