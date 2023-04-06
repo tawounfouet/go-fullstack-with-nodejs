d82a1314047f8b6e0d0ebd7dc5e1e4d58b16f9ed
Thomas AWOUNFOUET <thomas.awounfouet@gmail.com>
2023-04-06 21:45:29 +0200

    création de la base de donnée MongoDB et implémentation du CRUD

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,18 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+
+const Thing = require('./models/Thing');
+
+mongoose.connect('mongodb+srv://tawounfouet:jO3MQisIRZKUeorQ@cluster0.f4qe8yf.mongodb.net/test?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
 
 const app = express();
 
-app.use(express.json());
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
@@ -11,34 +21,83 @@ app.use((req, res, next) => {
     next();
   });
 
+// app.use(express.json());
+app.use(bodyParser.json());
+
+// app.post('/api/stuff', (req, res, next) => {
+//     console.log(req.body);
+//     res.status(201).json({         // code 201 = creation de ressources
+//         message: 'Objet créé !'
+//     });
+// });
+
 app.post('/api/stuff', (req, res, next) => {
-    console.log(req.body);
-    res.status(201).json({         // code 201 = creation de ressources
-        message: 'Objet créé !'
+    delete req.body._id;
+    const thing = new Thing({
+    //title: req.body.title,
+    ...req.body
+
     });
+    thing.save()
+        .then(() => res.status(201).json({ message: 'Objet enregistré !' }))
+        .catch(error => res.status(400).json({ error }));
 });
 
+
+
+// app.get('/api/stuff', (req, res, next) => {
+//     const stuff = [
+//       {
+//         _id: 'oeihfzeoi',
+//         title: 'Mon premier objet',
+//         description: 'Les infos de mon premier objet',
+//         imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
+//         price: 4900,
+//         userId: 'qsomihvqios',
+//       },
+//       {
+//         _id: 'oeihfzeomoihi',
+//         title: 'Mon deuxième objet',
+//         description: 'Les infos de mon deuxième objet',
+//         imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
+//         price: 2900,
+//         userId: 'qsomihvqios',
+//       },
+//     ];
+//     res.status(200).json(stuff);
+//   });
+
+
+app.put('/api/stuff/:id', (req, res, next) => {
+    Thing.updateOne({_id: req.params.id}, {...req.body, _id: req.params.id})
+        .then(() => res.status(200).json({ message: 'Objet modifié !'}))
+        .catch(error => res.status(400).json({ error })); // erreur 400 = mauvaise requete
+
+});
+
+
+app.delete('/api/stuff/:id', (req, res, next) => {
+    Thing.deleteOne({ _id: req.params.id })
+        .then(() => res.status(200).json({ message: 'Objet supprimé !'}))
+        .catch(error => res.status(400).json({ error }));
+});
+
+
+app.get('/api/stuff/:id', (req, res, next) => {
+    Thing.findOne({ _id: req.params.id })
+        .then(thing => res.status(200).json(thing))
+        .catch(error => res.status(404).json({ error })); // 404 = objet non trouvée
+    });
+
 app.get('/api/stuff', (req, res, next) => {
-    const stuff = [
-      {
-        _id: 'oeihfzeoi',
-        title: 'Mon premier objet',
-        description: 'Les infos de mon premier objet',
-        imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-        price: 4900,
-        userId: 'qsomihvqios',
-      },
-      {
-        _id: 'oeihfzeomoihi',
-        title: 'Mon deuxième objet',
-        description: 'Les infos de mon deuxième objet',
-        imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-        price: 2900,
-        userId: 'qsomihvqios',
-      },
-    ];
-    res.status(200).json(stuff);
+    Thing.find()
+        .then(things => res.status(200).json(things)) // promise
+        .catch(error => res.status(400).json({ error })); // erreur 400 = mauvaise requete
+   
   });
+
+
+
 
 
 module.exports = app;
